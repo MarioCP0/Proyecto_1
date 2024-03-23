7c9a26be7a56c3058acd2d0da71371c5ab488c23
PeDro0210 <pedroruben0210@gmail.com>
2024-03-22 22:42:51 -0600

    commenting evaluator

--- src/main/java/com/InterpreterClasses/Evaluator.java
+++ src/main/java/com/InterpreterClasses/Evaluator.java
@@ -7,21 +7,27 @@ import java.util.LinkedList;
 import com.ParsingEstructures.AST;
 import com.ParsingEstructures.Node;
 
-// TODO: needs a lot of fucking refactoring fr
-// TODO: add documentations to all of this
-public class Evaluator {
 
+public class Evaluator {
+    /*
+     *        Algo asi va ordenado la funcion
+     * function[nombre de funcion] = AST de la funcion
+     */
     private HashMap<String, AST<String>> functions;
+    /*
+     *      Ordenamiento de variable
+     * Variables[nombre de la variable] = lista linkeada con los valores
+     */
     private HashMap<String, LinkedList<String>> variables;
-    private HashMap<String, Integer> TimeVariableHaveBeenSet = new HashMap<String, Integer>();
-    private HashMap<String, String> functionVariables = new HashMap<String, String>();
+    private HashMap<String, Integer> TimeVariableHaveBeenSet = new HashMap<String, Integer>(); //Bastante simple solo (key:variable, value:en que iteracion de las seteada va)
+    private HashMap<String, String> functionVariables = new HashMap<String, String>(); //son los parametros de la funcion
 
     public Evaluator(HashMap<String, AST<String>> functions, HashMap<String, LinkedList<String>> variables) {
         this.functions = functions;
         this.variables = variables;
     }
 
-    public String evaluate(AST<String> ast) {
+    public String evaluate(AST<String> ast) { //mira el root para ver que clase de metodo es (casi todo lleva al default, podria cambiar esto, pero)
         Node<String> root = ast.getRoot();
         switch (root.getData()) {
             case "setq":    
@@ -38,23 +44,26 @@ public class Evaluator {
         return null; // resultado
     }
 
-    // TODO: Refactor the / and the - (it needs to intercalated one from the other)
     private String evaluateExpression(AST<String> ast) {
         Node<String> root = ast.getRoot();
         ArrayList<AST<String>> children = ast.getChildren();
-
-        if (functionVariables.containsKey(root.getData())) {
+        
+        if (functionVariables.containsKey(root.getData())) { // devuelve el valor del parametro de la funcion
             return functionVariables.get(root.getData());
         }
-        if (root.getData().equals("cond")){
+        if (root.getData().equals("cond")){ //llama a la funcion del condicional
             return EvaluatingCond(ast);
         }
-        if (functions.containsKey(root.getData())) {
+        if (functions.containsKey(root.getData())) { //evalua la funcion
             return EvaluatingFunction(functions.get(root.getData()), ast);
         }
-        if (variables.containsKey(root.getData())) {
+        if (variables.containsKey(root.getData())) { //devuelve el valor de la variable
             return variables.get(root.getData()).get(TimeVariableHaveBeenSet.get(root.getData()));
         }
+        /*
+         * Operaciones aritmeticas
+         * resultado (raiz de la operacion)= los hijos evaluados  #No esta tan bonito como los del parser
+         */
         if (root.getData().equals("+")) {
             float result = 0;
             for (AST<String> child : children) {
@@ -109,13 +118,24 @@ public class Evaluator {
             }
             return Float.toString(result);
         }
+        if (root.getData().equals("quote")|| root.getData().equals("'")) {
+            String NotEvaluated = "";
+            for (AST<String> child : children) {
+                NotEvaluated += child.getRoot().getData() + " ";
+            }
+            return NotEvaluated;
+        }
         return root.getData();
     }
 
-    // TODO: REFACTOR ALL THE COND (ONE TEST WORKS BUT NEEDS TO BE TESTED MORE)
     private String EvaluatingCond(AST<String> ast) {
         for (AST<String> child : ast.getChildren()){
             switch (child.getRoot().getData()) {
+                /*
+                 * Condicionales
+                 * t caso general #Como el default para los bobos
+                 * <, >, =, <=, >=, atom #casos especificos
+                 */
                 case "t":
                     return evaluateExpression(child.getChildren().get(0));
                 case "<":
@@ -141,6 +161,11 @@ public class Evaluator {
                 case ">=":
                     if (Float.parseFloat(evaluateExpression(child.getChildren().get(0))) >= Float.parseFloat(evaluateExpression(child.getChildren().get(1)))){
                         return evaluateExpression(child.getChildren().get(2));
+                    }
+                    break;
+                case "atom":
+                    if (evaluateExpression(child.getChildren().get(0)).matches("[0-9]+")){
+                        return evaluateExpression(child.getChildren().get(1));
                     }
                     break;
             }
